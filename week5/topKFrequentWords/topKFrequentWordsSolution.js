0f11b88a30fb308b2baebaf1944d8f3066352e82
ardydj <ardydj07@gmail.com>
2021-02-04 14:05:44 -0500

    1st Attempt Top K Frequent Words Problem

--- week5/topKFrequentWords/topKFrequentWordsSolution.js
+++ week5/topKFrequentWords/topKFrequentWordsSolution.js
@@ -49,7 +49,7 @@ const getFrequencies = (words) => {
         if(!freq.has(word)) freq.set(word, 0);
         freq.set(word, freq.get(word) + 1);
     });
-    
+
     return freq;
 };
 
@@ -71,9 +71,9 @@ const topKFrequent = (words, k) => {
     return ans;
 };
 
-
 // A HASH SOLUTION
 var topKFrequentHash = function (words, k) {
+    debugger;
     let hash = {};
     for (let word of words) {
         hash[word] = hash[word] + 1 || 1;
@@ -88,3 +88,5 @@ var topKFrequentHash = function (words, k) {
 };
 
 const bohemianRhapsody = ["is", "this", "the", "real", "life", "is", "this", "just", "fantasy", "caught", "in ", "a", "landslide", "no", "escape", "from", "reality", "open", "your", "eyes", "look", "up", "to", "the", "skies", "and", "see", "i`m", "just", "a", "poor", "boy,", "i", "need", "no", "sympathy", "because", "i`m", "easy", "come", "easy", "go", "a", "little", "high, ", "little", "low", "anyway", "the", "wind", "blows, ", "doesn`t", "really", "matter", "to", "me,", "to", "me", "mama, ", "just", "killed", "a", "man", "put", "a", "gun", "against", "his", "head", "pulled", "my", "trigger", "now", "he`s", "dead", "mama, ", "life", "had", "just", "begun", "but", "now", "i`ve", "gone", "and", "thrown", "it", "all", "away", "mama, ", "ooo", "didn`t", "mean", "to", "make", "you", "cry", "if", "i`m", "not", "back", "again", "this", "time", "tomorrow", "carry", "on, ", "carry", "on", "as", "if", "nothing", "really", "matters", "too", "late, ", "my", "time", "has", "come", "sends", "shivers", "down", "my", "spine", "body`s", "aching", "all", "the", "time", "goodbye", "everybody", "i`ve", "got", "to", "go", "gotta", "leave", "you", "all", "behind", "and", "face", "the", "truth", "mama, ", "ooo", "anyway", "the", "wind", "blows)", "i", "don`t", "want", "to", "die", "i", "sometimes", "wish", "i`d", "never", "been", "born", "at", "all", "i", "see", "a", "little", "silhouetto", "of", "a", "man", "scaramouch, ", "scaramouch", "will", "you", "do ", "the", "fandango", "thunderbolt", "and", "lightning", "very", "very", "frightening", "me", "gallileo, ", "gallileo,", "gallileo, ", "gallileo,", "gallileo", "figaro", "magnifico", "but", "i`m", "just", "a", "poor", "boy", "and", "nobody", "loves", "me", "he`s", "just", "a", "poor", "boy", "from", "a", "poor", "family", "spare", "him", "his", "life", "from", "this", "monstrosity", "easy", "come", "easy", "go", "will", "you", "let", "me", "go", "bismillah", "no", "we", "will", "not", "let", "you", "go", "let", "him", "go", "bismillah", "we", "will", "not", "let", "you", "go", "let", "him", "go", "bismillah", "we", "will", "not", "let", "you", "go", "let", "me", "go", "will", "not", "let", "you", "go", "let", "me", "go", "(never)", "never", "let", "you", "go", "let", "me", "go", "never", "let", "me", "go", "no", "no", "no", "no", "no", "no", "no", "oh", "mama", "mia", "mama", "mia", "mama", "mia", "let", "me", "go", "beelzebub", "has", "a", "devil", "put", "aside", "for", "me", "for", "me", "for", "me", "so", "you", "think", "you", "can", "stone", "me", "and", "spit", "in ", "my", "eye", "so", "you", "think", "you", "can", "love", "me", "and", "leave", "me", "to", "die", "oh", "baby", "can`t", "do", "this", "to", "me", "baby", "just", "gotta", "get", "out", "just", "gotta", "get", "right", "outta", "here", "ooh", "yeah, ", "ooh", "yeah", "nothing", "really", "matters", "anyone", "can", "see", "nothing", "really", "matters", "nothing", "really", "matters", "to", "me", "anyway", "the", "wind", "blows"];
+
+console.log(topKFrequentHash(bohemianRhapsody, 3));
