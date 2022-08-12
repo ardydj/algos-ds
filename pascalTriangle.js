928c9449db04e1ea9ce09d65f5185ee4402863aa
ardydj <ardydj07@gmail.com>
2022-08-11 21:47:14 -0700

    Add Pseudocode for Pascal's Triangle Implementation

--- pascalTriangle.js
+++ pascalTriangle.js
@@ -19,7 +19,11 @@
 */
 
 // if only left or only right, return parent value (assumption - the edges of each row will be 1 row[0] || row[row.length - 1])
+// assumption, each row has the same number of elements as the rowIndex
+// row 4 will need to calculate 4 elements recursively by checking the left and right parent
+// at each recursive call (recurrence relation), row.length -1
 var getRow = function(rowIndex) {
   //  base case
+  if () {}
   //
   };
