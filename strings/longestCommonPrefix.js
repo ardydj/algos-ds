095dae9b1c2d64ba04545cacfe7727aacf5f315f
ardydj <ardydj07@gmail.com>
2022-09-02 09:11:18 -0700

    LongestCommonPrefix 1st attempt

--- strings/longestCommonPrefix.js
+++ strings/longestCommonPrefix.js
@@ -2,4 +2,15 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function (strs) {};
+
+var longestCommonPrefix = function (strs) {
+  let longestPrefix = '';
+
+  for (let i = 0; i < strs.length - 1; i++) {
+    for (let j = i + 1; j <= strs.length - 1; j++) {
+      let currentChar = strs[i];
+    }
+  }
+
+  return longestPrefix;
+};
